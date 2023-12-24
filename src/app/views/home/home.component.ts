0d7064456b425fc82ab6f753f7e5ecbe42805edb
Jonathan Vicente <jonathan-priston@hotmail.com>
2023-12-24 12:35:02 -0300

    update titles when navigate application

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HeaderService } from './../../components/template/header/header.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
 
+export class HomeComponent implements OnInit{
+
+  constructor(private headerService: HeaderService){
+    headerService.headerData = {
+      title: 'Inicio',
+      icon: 'home',
+      routeUrl: ''
+    }
+
+   
+  }
+
+ 
+  ngOnInit(): void {
+    
+  }
 }
